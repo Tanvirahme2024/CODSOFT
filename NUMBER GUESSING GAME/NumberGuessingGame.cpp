d7612917f6869c631b95ee5a84a3f60d8c7bfd79
Tanvir Ahmed <ta7142047@gmail.com>
2025-03-22 19:20:05 +0600

    Add files via upload
    **Number Guessing Game**  
    
    This is a simple number guessing game where the program generates a random number, and the user has to guess it. The program provides hints by indicating whether the guessed number is too high or too low.  
    
    Learn more about generating random numbers in C++: [How To Generate Random Numbers](https://www.w3schools.com/cpp/cpp_howto_random_number.asp).

---
+++ NUMBER GUESSING GAME/NumberGuessingGame.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    srand(time(0));
+
+    int randomNum = rand();
+
+    cout << "Guess the Number:";
+    cin>>x;
+    if(x<randomNum)
+    {
+        cout<<"Too Low"<<endl;
+    }
+    else if(x>randomNum)
+    {
+        cout<<"Too High"<<endl;
+    }
+    else
+    {
+        cout<<"Your Guess Number is Right."<<endl;
+    }
+
+    return 0;
+}
+
